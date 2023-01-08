46488f78270189c8dff366c46c3fbce9a8cd5daa
Ning <920523764@qq.com>
2023-01-08 14:39:49 +0800

    Save records

--- src/main/java/demo/rainfallanalysis_beta/rainfallanalyser/RainfallAnalyser.java
+++ src/main/java/demo/rainfallanalysis_beta/rainfallanalyser/RainfallAnalyser.java
@@ -6,148 +6,102 @@ import com.opencsv.exceptions.CsvValidationException;
 import demo.rainfallanalysis_beta.rainfallanalyser.textio.TextIO;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class RainfallAnalyser {
-    public static void main(String[] args) {
-        System.out.print("Enter path name: ");
-        var path = TextIO.getln();
-
-        try {
-            TextIO.readFile(path);
-            String savePath = generateSavePath(path);
-            analyseDataset(savePath);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("ERROR: failed to process file");
-        }
-    }
-
-    //generic function to handle file open exceptions
-    static String processData(String path) {
-        String savePath = null;
-        try {
-            TextIO.readFile(path);
-            savePath = RainfallAnalyser.generateSavePath(path);
-            String flag = RainfallAnalyser.analyseDataset(savePath);
-            if (flag.equals("empty") || flag.equals("corrupt")) {
-                return "fail";
-            }
-        } catch (Exception e) {
-            //e.printStackTrace();
-            System.out.println("ERROR: failed to process file");
-        }
-        return savePath;
-    }
-
-    private static String generateSavePath(String path) {
-        var pathElements = path.trim().split("/");
-        var filenameElements = pathElements[2].trim().split("\\.");
-        return String.format("%s/%s_analysed.%s", pathElements[0],
-                filenameElements[0], filenameElements[1]);
-    }
-
-    private static String analyseDataset(String savePath) {
-        var header = extractRecord(); // ignore header record
-
-        if (header == null) {
-            //throw new IllegalArgumentException("ERROR: file is empty");
-            System.out.println("ERROR: file is empty");
-            return "empty";
-        }
-
-        // setup accumulation
-        var totalRainfall = 0.0;
-        var minRainfall = Double.POSITIVE_INFINITY;
-        var maxRainfall = 0.0;
-
-        var currentMonth = 1; // sentinel
-        var currentYear = 0; // sentinel
-
-        var record = extractRecord(); // get record
-
-        // setup output file
-        TextIO.writeFile(savePath);
-
-        // write header record
-        TextIO.putln("year,month,total,min,max");
-
-        // create new records for save file
-        while (record != null) {
-            // convert important fields to correct data types
-            var year = Integer.parseInt(record[2]);
-            var month = Integer.parseInt(record[3]);
-            var day = Integer.parseInt(record[4]);
-            var rainfall = record.length < 6 ? 0 : Double.parseDouble(record[5]);
-
-            if ((month < 1 || month > 12) || (day < 1 || day > 31)) {
-                System.out.println("ERROR: failed to process file");
-                return "corrupt";
-            }
-
-            if (month != currentMonth) {
-
-                // new month detected - save record and reset accumulation
-                writeRecord(totalRainfall, minRainfall, maxRainfall, currentMonth, currentYear == 0 ? year : currentYear);
-
-                currentMonth = month;
-                currentYear = year;
-                totalRainfall = 0;
-                minRainfall = Double.POSITIVE_INFINITY;
-                maxRainfall = 0;
-                continue;
-            }
-
-            // update accumulation
-            totalRainfall += rainfall;
-            if (rainfall < minRainfall) minRainfall = rainfall;
-            if (rainfall > maxRainfall) maxRainfall = rainfall;
-
-            record = extractRecord(); // get record
-        }
-
-        if (currentMonth < 12) {
-            // last month is incomplete - save record
-            writeRecord(totalRainfall, minRainfall, maxRainfall, currentMonth, currentYear);
-        }
-        return "success";
-    }
 
     //analyse dataset
+    //core function
     public static Boolean analyseData(File file){
-
         //open csv file
         try (CSVReader reader = new CSVReader(new FileReader(file))) {
+            //one line record
             String[] lineInArray;
+            ArrayList<String[]> records = new ArrayList<>();
+
+            //analyse dataset and save records
+            //data structure for lineInArray:
+            //[Product code, Bureau of Meteorology station number, Year, Month, Day, Rainfall amount (millimetres), Period over which rainfall was measured (days), Quality]
+            //[IDCJAC0009, 31205, 2001, 04, 30, 11.0, 1, N]
+
+            //data structure after processing (same as alpha version)
+            //[year, month, total, min, max]
+            //[2000,2, 1380.8, 0, 209.4]
+
+            // setup accumulation
+            var totalRainfall = 0.0;
+            var minRainfall = Double.POSITIVE_INFINITY;
+            var maxRainfall = 0.0;
+
+            var currentMonth = 1; // sentinel
+            var currentYear = 0; // sentinel
+
             while ((lineInArray = reader.readNext()) != null) {
-                //analyse dataset and save records
 
+                //ignore header
+                if (lineInArray[0].equalsIgnoreCase("product code") || lineInArray[0].equalsIgnoreCase("year")) {
+                    continue;
+                }
 
-                System.out.println(Arrays.toString(lineInArray));
+                // convert important fields to correct data types
+                var year = Integer.parseInt(lineInArray[2]);
+                var month = Integer.parseInt(lineInArray[3]);
+                var day = Integer.parseInt(lineInArray[4]);
+
+                //update check condition
+                var rainfall = 0.0;
+                if (!lineInArray[5].equals("")) {
+                    rainfall = Double.parseDouble(lineInArray[5]);
+                }
+                //var rainfall = lineInArray.length < 6 ? 0 : Double.parseDouble(lineInArray[5]);
+
+                if ((month < 1 || month > 12) || (day < 1 || day > 31)) {
+                    System.out.println("ERROR: failed to process file");
+                    return false;
+                }
+
+                if (month != currentMonth) {
+                    // new month detected - save record and reset accumulation
+                    records.add(writeRecord(totalRainfall, minRainfall, maxRainfall, currentMonth, currentYear == 0 ? year : currentYear));
+
+                    currentMonth = month;
+                    currentYear = year;
+                    totalRainfall = 0;
+                    minRainfall = Double.POSITIVE_INFINITY;
+                    maxRainfall = 0;
+                    continue;
+                }
+
+                // update accumulation
+                totalRainfall += rainfall;
+                if (rainfall < minRainfall) minRainfall = rainfall;
+                if (rainfall > maxRainfall) maxRainfall = rainfall;
+
             }
+
+            if (currentMonth < 12) {
+                // last month is incomplete - save record
+                records.add(writeRecord(totalRainfall, minRainfall, maxRainfall, currentMonth, currentYear));
+            }
+
+            //save updated records
+            FileHandleHelper.saveRecords(file.getName(), records);
+            System.out.println("Records save!");
+            return true;
+
         } catch (IOException | CsvValidationException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-
-
-
-        return true;
+        return false;
     }
 
 
-
-    private static void writeRecord(double totalRainfall, double minRainfall, double maxRainfall, int currentMonth, int year) {
-        var newRecord = String.format("%d,%d,%1.2f,%1.2f,%1.2f%s", year, currentMonth,
-                totalRainfall, minRainfall, maxRainfall, System.lineSeparator());
-        TextIO.putf(newRecord);
+    //updated writeRecord function
+    private static String[] writeRecord(double totalRainfall, double minRainfall, double maxRainfall, int currentMonth, int year) {
+        return new String[]{String.format("%d,%d,%1.2f,%1.2f,%1.2f%s", year, currentMonth,
+                totalRainfall, minRainfall, maxRainfall, System.lineSeparator())};
     }
 
-    private static String[] extractRecord() {
-        if (TextIO.eof()) return null; // convert EOF to null
-
-        var text = TextIO.getln();
-        return text.trim().split(",");
-    }
 }
